7858a9244b0dd3fff6b6a155be5642858bce03a5
Mo Shihab <Mohammedshihab6969@gmail.com>
2026-03-06 19:30:51 +0000

    Implement registration validation and processing logic

--- src/controllers/CreateRegister.ts
+++ src/controllers/CreateRegister.ts
@@ -1,7 +1,25 @@
 import { type Response, type Request } from 'express';
 import { processRegister } from '../services/register.service.js';
+import { AuthSchema } from '../validators/auth.schema.js';
 
 export const createRegister = async (req: Request, res: Response) => {
-  const result = await processRegister(req.body);
-  return res.status(200).json(result);
+  const safeInput = AuthSchema.safeParse(req.body);
+
+  if (!safeInput.success) {
+    return res.status(400).json({ error: safeInput.error });
+  }
+
+  if (safeInput.data.confirmPassword !== safeInput.data.password) {
+    return res
+      .status(400)
+      .json({ error: 'Password & Confirmed Password Do Not Match' });
+  }
+
+  const result = await processRegister(safeInput.data);
+
+  if (result?.sucess) {
+    return res.status(200).json({ message: result.message });
+  } else {
+    return res.status(400).json({ message: result?.message });
+  }
 };
--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,6 +1,46 @@
-export const processRegister = (data: any) => {
-  return {
-    success: true,
-    message: 'Logged in',
-  };
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import bycrypt from 'bcrypt';
+import { adapter } from '../../prisma/seed';
+import jwt from 'jsonwebtoken';
+
+type RegistrationData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const prisma = new PrismaClient({ adapter });
+
+export const processRegister = async (data: RegistrationData) => {
+  const { email, password } = data;
+
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+    const foundUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (foundUser) throw new Error('Email already in use.');
+
+    const hashedPassword = await bycrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: { email, password: hashedPassword },
+    });
+
+    if (newUser) {
+      let data = {
+        time: newUser.createdAt,
+        userId: newUser.id,
+      };
+      const token = jwt.sign(data, jwtSecretKey as string);
+      return { sucess: true, message: token };
+    }
+  } catch (error) {
+    return { sucess: true, message: error };
+  }
 };
